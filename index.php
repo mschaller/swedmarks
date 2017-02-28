1106f4a212473fb7ee4e2dfca56724d76ee68b03
Michael Schaller <michael.schaller@swednet.de>
2017-02-28 15:24:40 +0100

    clean index.php from folderTree code

--- index.php
+++ index.php
@@ -6,23 +6,5 @@ if(!isset($_SESSION["loggedin"])) {
     exit();
 }
 
-
-include "./config/config.php";
-$db = mysqli_connect($dsn['hostspec'],$dsn['username'],$dsn['password'],$dsn['database']);
-
-if(!$db)
-{
-  exit("Verbindungsfehler: ".mysqli_connect_error());
-}
-
-$result = mysqli_query($db, "SELECT id, childof as parent, name, user, deleted, public from folder where deleted <> 0");
-
-$rows = array();
-$count = 0;
-while(($row = mysqli_fetch_array($result))) {
-    $rows[$count++] = $row;
-}
 include "./config/configsmarty.php";
-$smarty->assign('name', 'Ned');
-$smarty->assign('rows', $rows);
 $smarty->display('index.tpl');
