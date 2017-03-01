93a4a8c73cc68911e357351a26cbc08c8053a5a8
Michael Schaller <michael.schaller@swednet.de>
2017-03-01 15:12:49 +0100

    add save bookmark code

--- getBookmark.php
+++ getBookmark.php
@@ -15,6 +15,32 @@ if($action != "new" && $action != "edit") {
 include "./config/config.php";
 if(isset($_POST["submit"])) {
     //todo: validate and write to database
+
+    $db = mysqli_connect($dsn['hostspec'],$dsn['username'],$dsn['password'],$dsn['database']);
+
+    if(!$db)
+    {
+        exit("Verbindungsfehler: ".mysqli_connect_error());
+    }
+    $query = sprintf(
+        "insert into bookmark
+            (user, title, url, description, childof, public)
+            values ('%s','%s','%s','%s','%s','%s')",
+        mysqli_real_escape_string($db, $_SESSION["user"]),
+        mysqli_real_escape_string($db, $_POST["title"]),
+        mysqli_real_escape_string($db, $_POST["url"]),
+        mysqli_real_escape_string($db, $_POST["description"]),
+        mysqli_real_escape_string($db, $_POST["folder"]),
+        "0"
+    );
+
+    if(mysqli_query($db, $query)) {
+        echo "ok";
+    } else {
+        echo "error";
+    }
+
+
     echo '<script language="JavaScript">reloadclose();</script>';
     echo '<script language="JavaScript">self.close();</script>';
     exit();
@@ -27,7 +53,7 @@ if($action == "new") {
     if(isset($_GET["folderid"])) {
         $folderid = $_GET["folderid"];
     }
-    $smarty->assign("folderid", $folderid);
+    $smarty->assign("bmfolderid", $folderid);
 } else {
     //todo: load bookmark-data
     //todo: assign smarty-vars
--- templates/bookmark.tpl
+++ templates/bookmark.tpl
@@ -1,12 +1,13 @@
 <div>
     <h2>{$title}</h2>
-    <form action="getBookmark.php?action={$action}&folder={$folderid}" id="dataForm" method="POST">
+    <form action="getBookmark.php?action={$action}" id="dataForm" method="POST">
         <p>Title<br>
         <input type=text name="title" size="50" value="{$bmtitle}"></p>
         <p>URL<br>
         <input type=text name="url" size="50" value="{$bmurl|default:"http://"}"></p>
         <p>Description<br>
         <textarea name="description" cols="50" rows="8">{$bmdescription}</textarea></p>
+        <input type="hidden" name="folder" value="{$bmfolderid}">
         <input type="submit" name="submit" value=" OK ">
         <input type="button" value=" Cancel " onClick="self.close()">
     </form>
