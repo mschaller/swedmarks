21a9a596ae577d555f9a10b865ab9837b457357a
Michael Schaller <michael.schaller@swednet.de>
2017-03-02 19:53:15 +0100

    add bookmark edit code

--- editBookmark.php
+++ editBookmark.php
@@ -14,32 +14,43 @@ if($action != "new" && $action != "edit") {
 
 include "./config/config.php";
 if(isset($_POST["submit"])) {
-    //todo: validate and write to database
-
     $db = mysqli_connect($dsn['hostspec'],$dsn['username'],$dsn['password'],$dsn['database']);
-
     if(!$db)
     {
         exit("Verbindungsfehler: ".mysqli_connect_error());
     }
-    $query = sprintf(
-        "insert into bookmark
-            (user, title, url, description, childof, public)
-            values ('%s','%s','%s','%s','%s','%s')",
-        mysqli_real_escape_string($db, $_SESSION["user"]),
-        mysqli_real_escape_string($db, $_POST["title"]),
-        mysqli_real_escape_string($db, $_POST["url"]),
-        mysqli_real_escape_string($db, $_POST["description"]),
-        mysqli_real_escape_string($db, $_POST["folder"]),
-        "0"
-    );
+    
+    if($action == "new") {
+        $query = sprintf(
+            "insert into bookmark
+                (user, title, url, description, childof, public)
+                values ('%s','%s','%s','%s','%s','%s')",
+            mysqli_real_escape_string($db, $_SESSION["user"]),
+            mysqli_real_escape_string($db, $_POST["title"]),
+            mysqli_real_escape_string($db, $_POST["url"]),
+            mysqli_real_escape_string($db, $_POST["description"]),
+            mysqli_real_escape_string($db, $_POST["folder"]),
+            "0"
+        );
 
+    } else {
+        $query = sprintf(
+            "update bookmark set title='%s', url='%s', description='%s', childof='%s'
+                where id='%s'",
+            mysqli_real_escape_string($db, $_POST["title"]),
+            mysqli_real_escape_string($db, $_POST["url"]),
+            mysqli_real_escape_string($db, $_POST["description"]),
+            mysqli_real_escape_string($db, $_POST["folder"]),
+            mysqli_real_escape_string($db, $_POST["bookmarkid"]) 
+        );
+
+    }
+    
     if(mysqli_query($db, $query)) {
         echo "ok";
     } else {
         echo "error";
     }
-
 
     echo '<script language="JavaScript">reloadclose();</script>';
     echo '<script language="JavaScript">self.close();</script>';
@@ -53,10 +64,37 @@ if($action == "new") {
     if(isset($_GET["folderid"])) {
         $folderid = $_GET["folderid"];
     }
+    $smarty->assign("bmtitle", "");
+    $smarty->assign("bmurl", "http://");
+    $smarty->assign("bmdescription", "");
+    $smarty->assign("bmid", "");
     $smarty->assign("bmfolderid", $folderid);
 } else {
-    //todo: load bookmark-data
-    //todo: assign smarty-vars
+    if(!isset($_GET["bookmarkid"])) {
+        exit("no bookmarkid");
+    }
+    $bookmarkid = $_GET["bookmarkid"];
+    $db = mysqli_connect($dsn['hostspec'],$dsn['username'],$dsn['password'],$dsn['database']);
+    if(!$db)
+    {
+        exit("Verbindungsfehler: ".mysqli_connect_error());
+    }
+
+    $result = mysqli_query($db, "SELECT * FROM bookmark where id = '"
+        .mysqli_real_escape_string($db, $bookmarkid)
+        ."'"
+    );
+
+    if($result->num_rows == 0) {
+        exit();
+    }
+    $row = mysqli_fetch_object($result);
+    
+    $smarty->assign("bmtitle", $row->title);
+    $smarty->assign("bmurl", $row->url);
+    $smarty->assign("bmdescription", $row->description);
+    $smarty->assign("bmfolderid", $row->childof);
+    $smarty->assign("bmid", $row->id);
 }
 
 $smarty->assign("action", $action);
--- templates/editbookmark.tpl
+++ templates/editbookmark.tpl
@@ -2,12 +2,13 @@
     <h2>{$title}</h2>
     <form action="editBookmark.php?action={$action}" id="dataForm" name="bmnew" method="POST">
         <p>Title<br>
-        <input type=text name="title" size="50" value="{$bmtitle}"></p>
+        <input type=text name="title" size="50" value="{$bmtitle|default:""}"></p>
         <p>URL<br>
         <input type=text name="url" size="50" value="{$bmurl|default:"http://"}"></p>
         <p>Description<br>
         <textarea name="description" cols="50" rows="8">{$bmdescription}</textarea></p>
         <input type="hidden" name="folder" value="{$bmfolderid}">
+        <input type="hidden" name="bookmarkid" value="{$bmid|default:""}">
         <input type="submit" name="submit" value=" OK ">
         <input type="button" value=" Cancel " onClick="self.close()">
     </form>
