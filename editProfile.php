b326042162c416184132699b3cecc8b7e462022b
Michael Schaller <michael.schaller@swednet.de>
2017-03-09 19:36:15 +0100

    add change password stuff

--- editProfile.php
+++ editProfile.php
@@ -8,22 +8,28 @@ if(!isset($_GET['action'])) {
     exit();
 }
 $action = strtolower($_GET['action']);
-if($action != "edit") {
+if($action != "edit" && $action != "changepw") {
     exit();
 }
 
 include "./config/config.php";
+
 if(isset($_POST["submit"])) {
     $db = mysqli_connect($dsn['hostspec'],$dsn['username'],$dsn['password'],$dsn['database']);
-
     if(!$db)
     {
         exit("Verbindungsfehler: ".mysqli_connect_error());
     }
-//    $query = sprintf("",
-//        //mysqli_real_escape_string($db, $_POST["folder"]),
-//        //mysqli_real_escape_string($db, $_SESSION["user"])
-//    );
+
+    if($action == "edit") {
+    }
+    else {
+        $query = sprintf("update user set password = md5('%s') where user = '%s' and password = md5('%s')",
+            mysqli_real_escape_string($db, $_POST["password1"]),
+            mysqli_real_escape_string($db, $_SESSION["user"]),
+            mysqli_real_escape_string($db, $_SESSION["password0"])
+        );
+    }
 
     if(mysqli_query($db, $query)) {
         echo "ok";
@@ -31,9 +37,8 @@ if(isset($_POST["submit"])) {
         echo "error";
     }
 
-
     //echo '<script language="JavaScript">window.opener.updateFolderTree();</script>';
-    echo '<script language="JavaScript">self.close();</script>';
+    //echo '<script language="JavaScript">self.close();</script>';
     exit();
 }
 
--- templates/profile.tpl
+++ templates/profile.tpl
@@ -9,9 +9,22 @@
     <h2><img src="assets/person.svg" class="icon24"/>{$user}'s profile</h2>
     <div>
         <h3>User data</h3>
-        <form action="editProfile.php?action={$action}" id="dataForm" name="profile" method="POST">
+        <form action="editProfile.php?action=edit" id="dataForm" name="profiledata" method="POST">
         </form>
     </div>
+    <div>
+        <h3>Change password</h3>
+        <form action="editProfile.php?action=changepw" id="dataForm" name="profilepassword" method="POST">
+            <span style="display: inline-block; width: 120px;"><label for="password">Old password</label></span>
+            <span><input id="password0" name="password0" type="password"/></span><br/>
+            <span style="display: inline-block; width: 120px;"><label for="password">New Password</label></span>
+            <span><input id="password1" name="password1" type="password"/></span><br/>
+            <span style="display: inline-block; width: 120px;"><label for="password">Repeat it</label></span>
+            <span><input id="password2" name="password2" type="password"/></span><br/>
+            <div><input type="submit" value="change password" name="profilepwd"/></div>
+        </form>
+    </div>
+
     <div>
         <h3>Bookmarklet</h3>
         <div>
